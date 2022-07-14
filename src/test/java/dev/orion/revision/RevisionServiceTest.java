2786c7fa265e77a2da5e1f8d9a7c903f7c326a38
Antonio <as@cfgroup.com.br>
2022-07-14 16:34:40 -0300

    Comenta teste errado

--- src/test/java/dev/orion/revision/RevisionServiceTest.java
+++ src/test/java/dev/orion/revision/RevisionServiceTest.java
@@ -32,6 +32,7 @@ class RevisionServiceTest {
             //.body(is("Rodrigo Prestes Machado"))
     }
 
+    /*     
     @Test
     @DisplayName("Test wrong user")
     @Order(2)
@@ -47,7 +48,7 @@ class RevisionServiceTest {
             .statusCode(400);
      }
      
-    /*     @Test
+    @Test
     @DisplayName("Test wrong Moodle user")
     @Order(2)
     void wrongMoodleUser() {
