32060921232a9cbbce7cfe38319ea1f721e5fbd0
Alexandre Fabian <xandy.fabian@gmail.com>
2022-08-10 16:54:45 -0300

    add mapper/repository test

---
+++ src/test/java/dev/orion/revision/RepositoryTest.java
@@ -0,0 +1,47 @@
+package dev.orion.revision;
+
+
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import dev.orion.revision.clients.Github;
+import dev.orion.revision.mappers.github.Repository;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class RepositoryTest {
+    
+   
+    Repository repo;
+
+    @Inject
+    @RestClient
+    protected Github github;
+    
+    @Test
+    @DisplayName("Repository Mapper Test isFork")
+    @Order(1)
+    public void testRepositoryisFork(){
+        repo = github.getRepo("graziellarodrigues", "cpw2-web-storage");
+        Assertions.assertEquals(true, repo.isFork());
+    } 
+    @Test
+    @DisplayName("Repository Mapper Test id")
+    @Order(2)
+    public void testRepositoryId(){
+        repo = github.getRepo("graziellarodrigues", "cpw2-web-storage");
+        Assertions.assertEquals("438989221", repo.getId());
+    } 
+    @Test
+    @DisplayName("Repository Mapper Test Parent")
+    @Order(3)
+    public void testRepositoryParent(){
+        repo = github.getRepo("graziellarodrigues", "cpw2-web-storage");
+        Assertions.assertEquals("435871324", repo.getParent().getId());
+    } 
+}
